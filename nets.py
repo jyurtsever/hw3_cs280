c92ec12e0cac7a683b01d27cf4b99f83c853fc0a
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-03-25 11:34:11 -0700

    added command line args

--- nets.py
+++ nets.py
@@ -47,7 +47,7 @@ class BasicBlock(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False):
+    def __init__(self, block, layers, num_classes=10, zero_init_residual=False):
         super(ResNet, self).__init__()
         self.inplanes = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
