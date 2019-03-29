47ca54b26d12f0e3fd536e3eb6ae6c9cd576c7ce
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-03-29 12:08:31 -0700

    added droupout instead of changing optimizer

--- nets.py
+++ nets.py
@@ -106,7 +106,7 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        x = self.dropout(x)
+        # x = self.dropout(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
