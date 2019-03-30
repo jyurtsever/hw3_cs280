f9100812283bbeab2f0a3de68c26a240a1026cd0
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-03-29 21:39:49 -0700

    added resnet3

--- nets.py
+++ nets.py
@@ -237,7 +237,7 @@ class ResNet3(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         # x = self.layer4(x)
-        # x = self.avgpool(x)
+        x = self.avgpool(x)
         # x = x.view(x.size(0), -1)
         if self.dropout is not None:
             x = self.dropout(x)
